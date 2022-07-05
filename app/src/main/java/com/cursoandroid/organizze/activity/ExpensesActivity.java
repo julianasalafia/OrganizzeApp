2a2635fb0c0aea030e65a762a3104a3b9b73c6a0
Juliana Salafia <juliana.salafia1@gmail.com>
2022-07-05 10:29:02 -0300

    recover total expense

--- app/src/main/java/com/cursoandroid/organizze/activity/ExpensesActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/ExpensesActivity.java
@@ -8,14 +8,25 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cursoandroid.organizze.R;
+import com.cursoandroid.organizze.config.ConfigurationFirebase;
+import com.cursoandroid.organizze.helper.Base64Custom;
 import com.cursoandroid.organizze.helper.DateCustom;
 import com.cursoandroid.organizze.model.Transaction;
+import com.cursoandroid.organizze.model.User;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class ExpensesActivity extends AppCompatActivity {
     private TextInputEditText fieldDate, fieldCategory, fieldDescription;
     private EditText fieldAmount;
     private Transaction transaction;
+    private DatabaseReference firebaseRef = ConfigurationFirebase.getFirebaseDatabase();
+    private FirebaseAuth auth = ConfigurationFirebase.getFirebaseAuth();
+    private Double totalExpense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +39,23 @@ public class ExpensesActivity extends AppCompatActivity {
         fieldAmount = findViewById(R.id.editAmount);
 
         fieldDate.setText(DateCustom.currentData());
+        recoverTotalExpense();
     }
 
     public void saveExpense(View view) {
         if (validateExpenseField()) {
             transaction = new Transaction();
             String date = fieldDate.getText().toString();
-            transaction.setAmount(Double.parseDouble(fieldAmount.getText().toString()));
+            Double recoveredAmount = Double.parseDouble(fieldAmount.getText().toString());
+
+            transaction.setAmount(recoveredAmount);
             transaction.setCategory(fieldCategory.getText().toString());
             transaction.setDescription(fieldDescription.getText().toString());
             transaction.setDate(date);
             transaction.setType("e");
+
+            Double updatedExpense = totalExpense + recoveredAmount;
+            updateExpenses(updatedExpense);
 
             transaction.save(date);
         }
@@ -79,5 +96,32 @@ public class ExpensesActivity extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
             return false;
         }
+    }
+
+    public void recoverTotalExpense() {
+        String emailUser = auth.getCurrentUser().getEmail();
+        String idUser = Base64Custom.encode64Base(emailUser);
+        DatabaseReference userRef = firebaseRef.child("users").child(idUser);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                totalExpense = user.getTotalExpense();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void updateExpenses(Double expense) {
+        String emailUser = auth.getCurrentUser().getEmail();
+        String idUser = Base64Custom.encode64Base(emailUser);
+        DatabaseReference userRef = firebaseRef.child("users").child(idUser);
+
+        userRef.child("totalExpense").setValue(expense);
     }
 }
