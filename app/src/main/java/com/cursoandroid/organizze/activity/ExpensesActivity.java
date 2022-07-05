c5018bcf0bd3ce7be1cd4135f9d1f365ff682246
Juliana Salafia <juliana.salafia1@gmail.com>
2022-07-05 09:51:37 -0300

    validate expenses fields

--- app/src/main/java/com/cursoandroid/organizze/activity/ExpensesActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/ExpensesActivity.java
@@ -3,6 +3,7 @@ package com.cursoandroid.organizze.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,14 +31,53 @@ public class ExpensesActivity extends AppCompatActivity {
     }
 
     public void saveExpense(View view) {
-        transaction = new Transaction();
-        String date = fieldDate.getText().toString();
-        transaction.setAmount(Double.parseDouble(fieldAmount.getText().toString()));
-        transaction.setCategory(fieldCategory.getText().toString());
-        transaction.setDescription(fieldDescription.getText().toString());
-        transaction.setDate(date);
-        transaction.setType("e");
+        if (validateExpenseField()) {
+            transaction = new Transaction();
+            String date = fieldDate.getText().toString();
+            transaction.setAmount(Double.parseDouble(fieldAmount.getText().toString()));
+            transaction.setCategory(fieldCategory.getText().toString());
+            transaction.setDescription(fieldDescription.getText().toString());
+            transaction.setDate(date);
+            transaction.setType("e");
 
-        transaction.save(date);
+            transaction.save(date);
+        }
+    }
+
+    public Boolean validateExpenseField() {
+        String textAmount = fieldAmount.getText().toString();
+        String textDate = fieldDate.getText().toString();
+        String textCategory = fieldCategory.getText().toString();
+        String textDescription = fieldDescription.getText().toString();
+
+        if (!textAmount.isEmpty()) {
+            if (!textDate.isEmpty()) {
+                if (!textCategory.isEmpty()) {
+                    if (!textDescription.isEmpty()) {
+                        return true;
+                    } else {
+                        Toast.makeText(ExpensesActivity.this,
+                                "Give us a description",
+                                Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                } else {
+                    Toast.makeText(ExpensesActivity.this,
+                            "What category is it from?",
+                            Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            } else {
+                Toast.makeText(ExpensesActivity.this,
+                        "When did you make the transaction?",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        } else {
+            Toast.makeText(ExpensesActivity.this,
+                    "What's the amount?",
+                    Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
