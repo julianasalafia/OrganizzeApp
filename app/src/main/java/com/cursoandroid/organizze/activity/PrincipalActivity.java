de2981b090403aafbd78805939f0f88ba42ff782
Juliana Salafia <juliana.salafia1@gmail.com>
2022-07-14 18:28:29 -0300

    adding calendar to principal activity

--- app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
@@ -3,13 +3,20 @@ package com.cursoandroid.organizze.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.cursoandroid.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+    private MaterialCalendarView calendarView;
+    private TextView textGreeting;
+    private TextView textBalance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,11 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        calendarView = findViewById(R.id.calendarView);
+        textGreeting = findViewById(R.id.textGreeting);
+        textBalance = findViewById(R.id.textBalance);
+        configureCalendarView();
 
         /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +46,17 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void addExpense(View view) {
         startActivity(new Intent(this, ExpensesActivity.class));
+    }
+
+    public void configureCalendarView() {
+        CharSequence months[] =
+                {"January","February","March", "April", "May", "June ", "July ", "August", "September", "October", "November", "December"};
+                calendarView.setTitleMonths(months);
+                calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+                    @Override
+                    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+                    }
+                });
     }
 }
