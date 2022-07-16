eabd9f1512612ce0462ec1784acf4241dfd639d8
Juliana Salafia <juliana.salafia1@gmail.com>
2022-07-16 13:36:30 -0300

    config menu toolbar

--- app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/cursoandroid/organizze/activity/PrincipalActivity.java
@@ -2,13 +2,18 @@ package com.cursoandroid.organizze.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.cursoandroid.organizze.R;
+import com.cursoandroid.organizze.config.ConfigurationFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -17,12 +22,14 @@ public class PrincipalActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
     private TextView textGreeting;
     private TextView textBalance;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
 
         calendarView = findViewById(R.id.calendarView);
@@ -40,6 +47,26 @@ public class PrincipalActivity extends AppCompatActivity {
         });*/
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.closeMenu:
+                auth = ConfigurationFirebase.getFirebaseAuth();
+                auth.signOut();
+
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void addIncome(View view) {
         startActivity(new Intent(this, IncomesActivity.class));
     }
@@ -50,13 +77,13 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void configureCalendarView() {
         CharSequence months[] =
-                {"January","February","March", "April", "May", "June ", "July ", "August", "September", "October", "November", "December"};
-                calendarView.setTitleMonths(months);
-                calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
-                    @Override
-                    public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                {"January", "February", "March", "April", "May", "June ", "July ", "August", "September", "October", "November", "December"};
+        calendarView.setTitleMonths(months);
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
 
-                    }
-                });
+            }
+        });
     }
 }
